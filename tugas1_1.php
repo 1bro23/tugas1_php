8fd63385135d2993ecd675440b4e6407f68ac05e
1bro23 <shinlia21@gmail.com>
2021-08-09 10:14:11 +0800

    add tugas 1 php

---
+++ tugas1_1.php
@@ -0,0 +1,50 @@
+<?php
+  $ag=13000;
+  $ab=15000;
+  $as=13000;
+  $teg=1000;
+  $tag=1000;
+  $s=3000;
+  $np=5000;
+  $eteh=4000;
+  $eteb=5000;
+  echo "<h1>Daftar Menu</h1>";
+  echo "<table>
+          <tr>
+            <td><li>Ayam Goreng</li></td>
+            <td>= Rp. $ag</td>
+          </tr>
+          <tr>
+            <td><li>Ayam Bakar</li></td>
+            <td>= Rp. $ab</td>
+          </tr>
+          <tr>
+            <td><li>Ayam Sayur</li></td>
+            <td>= Rp. $as</td>
+          </tr>
+          <tr>
+            <td><li>Tempe Goreng</li></td>
+            <td>= Rp. $teg</td>
+          </tr>
+          <tr>
+            <td><li>Tahu Goreng</li></td>
+            <td>= Rp. $tag</td>
+          </tr>
+          <tr>
+            <td><li>Soto</li></td>
+            <td>= Rp. $s</td>
+          </tr>
+          <tr>
+            <td><li>Nasi Putih</li></td>
+            <td>= Rp. $np</td>
+          </tr>
+          <tr>
+            <td><li>Es Teh</li></td>
+            <td>= Rp. $eteh</td>
+          </tr>
+          <tr>
+            <td><li>Es Tebu</li></td>
+            <td>= Rp. $eteb</td>
+          </tr>
+        </table>";
+ ?>
---
+++ tugas1_2.php
@@ -0,0 +1,40 @@
+<?php
+  $c=1000;
+  $ed=15000;
+  $em=20000;
+  $et=5000;
+  $p=1000;
+  echo "<h1>DAFTAR MENU</h1>";
+  echo "<table border=1>
+          <tr>
+            <th>No</th>
+            <th>Nama Menu</th>
+            <th>Harga</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Cireng</td>
+            <td>Rp. $c</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Es Duren</td>
+            <td>Rp. $ed</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Es Milo</td>
+            <td>Rp. $em</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Es Teh</td>
+            <td>Rp. $et</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Piscok</td>
+            <td>Rp. $p</td>
+          </tr>
+        </table>";
+ ?>
